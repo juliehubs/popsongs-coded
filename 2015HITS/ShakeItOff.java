397e1eb2f65d9942a193ea35256285376c55fb61
juliehubs <juliehubschman@gmail.com>
2015-09-12 11:18:26 -0400

    Create ShakeItOff.java
    Shake It Off - Taylor Swift in JAVA

---
+++ 2015HITS/ShakeItOff.java
@@ -0,0 +1,31 @@
+public class ShakeItOff {
+    public static void main(String[] args){
+        chorus();
+        
+    }
+
+	private static void chorus() {
+		String gonna;
+		String person;
+		String[] verbs = {"play","hate","shake","shake it off","break","fake","shake"};
+		for(int i = 0; i<verbs.length; i ++){
+			if(verbs[i] == "shake it off"){
+				person = "I";
+				gonna = "shake it off";
+				gonna = "shake it off";
+				break;
+			}
+			person = verbs[i]+"er";
+			for(int j = 0; j<5; j++){
+				gonna = verbs[i];
+			}
+			
+			
+			
+		}
+		person = "I";
+		gonna = "shake it off";
+		gonna = "shake it off";
+		
+	}
+    }
